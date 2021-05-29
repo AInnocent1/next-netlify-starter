739433a95f67480e916836c31f4693997aa4f565
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-29 07:23:45 -0500

    Update startpage with hello world

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hello World!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello Conor and other world citizens!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
