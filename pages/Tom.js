41a09a47055c2ef9abb4c0ae5e9ff554505ac65e
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-30 01:30:06 -0500

    Update Tom.js

--- pages/Tom.js
+++ pages/Tom.js
@@ -1,21 +1,26 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-
-
+import Footer from '@components/Footer'
+import Canvas from '@components/Canvas'
+import React from 'react'
 export default function Home() {
   return (
-    <div className="container">
+      <div className="container" width="100%" height="100%">
+   
       <Head>
         <title>Hello World!</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="This will be a hub for TOM Activities" />
+        <Header title="Hello Conor and other world citizens!"/>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        
       </main>
+     <div className="canvas">
+        <canvas id="c" width="100%" height="100%"></canvas>
+        </div>
     </div>
-  )
-}
+  
+    )}
