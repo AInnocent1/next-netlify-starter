5c9fbb933beba21accd8600a1d055c3dc1f0ef0e
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-29 22:35:24 -0500

    Insert div with blank canvas in

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Canvas from '@components/Canvas'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Hello World!</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
@@ -15,7 +15,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+      <
       </main>
     </div>
+    <div className="Canvas">
+    <Canvas></>
   )
 }
