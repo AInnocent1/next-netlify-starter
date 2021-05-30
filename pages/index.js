0f9083ec551ae1cdc67a94deaf8bff4f463a1ba6
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-29 23:20:25 -0500

    rollback to working

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,20 @@ import Canvas from '@components/Canvas'
 
 export default function Home() {
   return (
+      <div className="container">
+    <div className="canvas">
+        <canvas id="canvas" width="100%" height="100%"></canvas>
+        </div>
       <Head>
         <title>Hello World!</title>
       </Head>
 
       <main>
         <Header title="Hello Conor and other world citizens!"/>
-        </main>
-    
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+        
+      </main>
+    </div>
     )}
