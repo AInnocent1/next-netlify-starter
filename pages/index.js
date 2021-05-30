2bfa00d0ecf38394a581cca78e6f696bf3729a4e
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-29 23:13:16 -0500

    remove canvas

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,6 @@ export default function Home() {
   return (
     
     <div className="container">
-    <div className="canvas">
-    <canvas id="canvas" width="100%" height="100%"></canvas>
-    </div>
       <Head>
         <title>Hello World!</title>
       </Head>
