0104746b3758251a055591d180c56ec684eeec19
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-29 21:53:14 -0500

    delete canvas, roll back to find what works

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Canvas from '@components/Canvas'
 
 export default function Home() {
   return (
@@ -16,9 +15,6 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-    <body style="background:black; width:100%; height:100%">
-        <canvas id="c" style="display: block;"></canvas>
-    </body>
       </main>
     </div>
   )
