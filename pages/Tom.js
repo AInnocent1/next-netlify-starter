9c64451cdcd93ca951f27c85835f5cccd57e7671
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-30 01:36:45 -0500

    try again to merge in canvas app

--- pages/Tom.js
+++ pages/Tom.js
@@ -1,6 +1,30 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Canvas from '@components/Canvas'
 import React from 'react'
+export default function Tom() {
+  return (
+      <div className="container" width="100%" height="100%">
+   
+      <Head>
+        <title>Hello World!</title>
+      </Head>
 
-export default function App() {
+      <main>
+        <Header title="Hello Conor and other world citizens!"/>
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+        
+      </main>
+     <div className="canvas">
+        <canvas id="c" width="100%" height="100%"></canvas>
+        </div>
+    </div>
+    
+
+function App() {
   const canvasRef = React.useRef(null)
   return (
     <canvas
@@ -14,3 +38,6 @@ export default function App() {
 }
 
 export default App
+
+  
+    )}
