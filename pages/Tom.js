5669242ec538afc8f9ba6bd923657075d8639fa5
AInnocent1 <39953095+AInnocent1@users.noreply.github.com>
2021-05-30 01:31:43 -0500

    Change tom entirely into canvas

--- pages/Tom.js
+++ pages/Tom.js
@@ -1,26 +1,16 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Canvas from '@components/Canvas'
 import React from 'react'
-export default function Home() {
-  return (
-      <div className="container" width="100%" height="100%">
-   
-      <Head>
-        <title>Hello World!</title>
-      </Head>
 
-      <main>
-        <Header title="Hello Conor and other world citizens!"/>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        
-      </main>
-     <div className="canvas">
-        <canvas id="c" width="100%" height="100%"></canvas>
-        </div>
-    </div>
-  
-    )}
+function App() {
+  const canvasRef = React.useRef(null)
+  return (
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={e => {        const canvas = canvasRef.current        const ctx = canvas.getContext('2d')        // implement draw on ctx here
+      }}
+    />
+  )
+}
+
+export default App
